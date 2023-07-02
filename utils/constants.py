a7aafcd2763478af34a8d33e8475f5cad7612c08
Mattis-Schulte <MattisSchulte@hotmail.de>
2023-07-02 21:57:30 +0200

    updated preprompt

--- utils/constants.py
+++ utils/constants.py
@@ -6,7 +6,7 @@ SENTIMENTS = {
     "british": {"display_name": "British", "prompt_suffix": "in the style of Shakespeare"},
     "ebonics": {"display_name": "Ebonics", "prompt_suffix": "with a heavy use of Ebonics"},
     "egirl": {"display_name": "E-Girl", "prompt_suffix": "like an E-Girl"},
-    "old": {"display_name": "Old Man", "prompt_suffix": "like an old conservative and cynical old man"},
+    "old": {"display_name": "Old Man", "prompt_suffix": "like a conservative and cynical old man"},
     "uwu": {"display_name": "UwU", "prompt_suffix": "in UwU language"}
 }
 DEFAULT_SENTIMENT = "wise"
